a20c7723f1bd8c61fdb152bccbdc0f38a6d1d24d
lukovana <astaffers@gmail.com>
2019-06-14 07:00:14 -0700

    Updates

---
+++ precache-manifest.00c4d0e535739bc47ca7c27125ced1a1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "db52bdd30de7fd05f8a8c0688ac1865d",
+    "url": "/whfah/static/media/Staatliches-Regular.db52bdd3.ttf"
+  },
+  {
+    "revision": "1e259881e6e6c7ebac16",
+    "url": "/whfah/static/css/main.3e41f9bd.chunk.css"
+  },
+  {
+    "revision": "8ba8de9a7896ff9d30eb",
+    "url": "/whfah/static/js/runtime~main.0ba8d181.js"
+  },
+  {
+    "revision": "ce3f2561adfd1a4d771d",
+    "url": "/whfah/static/js/2.10cb4975.chunk.js"
+  },
+  {
+    "revision": "78b50d8f2b76211b830a4e9411b86e24",
+    "url": "/whfah/static/media/Satisfy-Regular.78b50d8f.ttf"
+  },
+  {
+    "revision": "a57bf31d5e94a758c57f328e9ca3270f",
+    "url": "/whfah/static/media/Cookie-Regular.a57bf31d.ttf"
+  },
+  {
+    "revision": "1e259881e6e6c7ebac16",
+    "url": "/whfah/static/js/main.57d00186.chunk.js"
+  },
+  {
+    "revision": "ac5543cf30f1e464c0ae46bbbe2f9f9d",
+    "url": "/whfah/static/media/Adlinnaka-BoldDemo.ac5543cf.ttf"
+  },
+  {
+    "revision": "5a798cdadc7cd321e3f72425b70bface",
+    "url": "/whfah/static/media/OpenSans-Regular.5a798cda.ttf"
+  },
+  {
+    "revision": "0062c34665a3fc0f2278cd4e955702ec",
+    "url": "/whfah/static/media/OpenSans-Bold.0062c346.ttf"
+  },
+  {
+    "revision": "81bf90c999c04923ac320168abfc5d44",
+    "url": "/whfah/static/media/fridge.81bf90c9.jpg"
+  },
+  {
+    "revision": "315e67e42e11297d3014aa094dc663af",
+    "url": "/whfah/index.html"
+  }
+];
